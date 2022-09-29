9c9f0990b336007502df845146dd5d2aa1f81c17
lucas f. silva <lucasdotfs@gmail.com>
2022-09-29 09:16:59 -0300

    feat: add flashcard context

---
+++ src/contexts/FlashcardContext.js
@@ -0,0 +1,27 @@
+import { useState, useContext } from "react";
+import FLASHCARDS from "../mock/flashcardsMock";
+
+export const FlashCardContext = useContext();
+
+const FlashCardProvider = ({ children }) => {
+  const [flashcards, setFlashCards] = useState(FLASHCARDS);
+
+  const updateDoneStatus = (id, doneStatus) => {
+    if (flashcards["id"] === id) {
+      const updatedFlashCards = flashcards.map((flashcard) =>
+        flashcard["id"] == id
+          ? (flashcard.doneStatus = doneStatus)
+          : (flashcard.doneStatus = flashcard.doneStatus)
+      );
+      setFlashCards(updatedFlashCards);
+    }
+  };
+
+  return (
+    <FlashCardContext.Provider value={{ flashcards, updateDoneStatus }}>
+      {children}
+    </FlashCardContext.Provider>
+  );
+};
+
+export default FlashCardProvider;
