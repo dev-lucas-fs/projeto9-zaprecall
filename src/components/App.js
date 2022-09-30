828d253ae3ccb77ac4bb7cb7e311823d9f7e821c
lucas f. silva <lucasdotfs@gmail.com>
2022-09-30 14:51:47 -0300

    feat: add ThemeProvider on App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import FlashCardProvider from "../contexts/FlashcardContext";
+import { theme } from "../styles/styles";
+import Done from "./Done";
+import FlashCards from "./flashcard/FlashCards";
+import styled, { ThemeProvider } from "styled-components";
 
 export default function App() {
   return (
     <FlashCardProvider>
-      <div>App</div>
+      <ThemeProvider theme={theme}>
+        <div>
+          <FlashCards />
+          <Done />
+        </div>
+      </ThemeProvider>
     </FlashCardProvider>
   );
 }
